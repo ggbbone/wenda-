a071e06d58e11a40e1271144d8695cece6b83ae6
ggbbone <yang847857204@gmail.com>
2019-07-11 12:40:17 +0800

    评论

---
+++ src/main/java/com/yzg/toutiao/controller/CommentController.java
@@ -0,0 +1,12 @@
+package com.yzg.toutiao.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author yzg
+ * @create 2019/7/9
+ */
+@Controller
+public class CommentController {
+
+}
---
+++ src/main/java/com/yzg/toutiao/service/CommentService.java
@@ -0,0 +1,13 @@
+package com.yzg.toutiao.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * @author yzg
+ * @create 2019/7/9
+ */
+@Service
+@Transactional
+public class CommentService {
+}
