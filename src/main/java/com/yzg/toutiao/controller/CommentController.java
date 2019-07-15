96d758ecbdc213d186dd9b36d16c0e09355c1e5e
ggbbone <yang847857204@gmail.com>
2019-07-15 09:04:27 +0800

    增加评论发布功能

--- src/main/java/com/yzg/toutiao/controller/CommentController.java
+++ src/main/java/com/yzg/toutiao/controller/CommentController.java
@@ -1,12 +1,48 @@
 package com.yzg.toutiao.controller;
 
+import com.yzg.toutiao.model.Comment;
+import com.yzg.toutiao.model.HostHolder;
+import com.yzg.toutiao.model.Result;
+import com.yzg.toutiao.service.CommentService;
+import org.apache.catalina.Host;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * @author yzg
  * @create 2019/7/9
  */
 @Controller
+@RequestMapping(value = "comments")
 public class CommentController {
+    @Autowired
+    CommentService commentService;
+    @Autowired
+    HostHolder hostHolder;
 
+    @RequestMapping(value = "/question/{questionId}", method = RequestMethod.GET)
+    @ResponseBody
+    public Result getCommentsByQuestionId(
+            @PathVariable(value = "questionId") int questionId,
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "5", required = false) int limit) {
+        List<Comment> comments = commentService.getCommentsByQuestionId(questionId, offset, limit);
+
+        return new Result().success().data(comments);
+    }
+
+    @RequestMapping(value = "/question/{questionId}", method = RequestMethod.POST)
+    @ResponseBody
+    public Result insertCommentWithQuestion(
+            @PathVariable(value = "questionId") int questionId,
+            @RequestParam String content) {
+        if (hostHolder.getUser() != null) {
+            commentService.insertComment(questionId, content, hostHolder.getUser().getId());
+        }
+        return new Result().success();
+    }
 }
--- src/main/java/com/yzg/toutiao/service/CommentService.java
+++ src/main/java/com/yzg/toutiao/service/CommentService.java
@@ -1,7 +1,15 @@
 package com.yzg.toutiao.service;
 
+import com.github.pagehelper.PageHelper;
+import com.yzg.toutiao.dao.CommentMapper;
+import com.yzg.toutiao.model.Comment;
+import com.yzg.toutiao.model.example.CommentExample;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
 
 /**
  * @author yzg
@@ -10,4 +18,44 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class CommentService {
+
+    @Autowired
+    CommentMapper commentMapper;
+
+    /**
+     * 插入一条回答
+     *
+     * @param entityId
+     * @param content
+     * @param userId
+     */
+    public void insertComment(int entityId, String content, int userId) {
+        Comment comment = new Comment();
+        comment.setContent(content);
+        comment.setUserId(userId);
+        comment.setCreadtedDate(new Date());
+        comment.setEntityId(entityId);
+        //评论类型，1表示问题的回答
+        comment.setEntityType(1);
+        commentMapper.insertSelective(comment);
+    }
+
+    /**
+     * 查询某个问题的回答
+     *
+     * @param questionId
+     * @param offset
+     * @param limit
+     * @return
+     */
+    public List<Comment> getCommentsByQuestionId(int questionId, int offset, int limit) {
+
+        CommentExample commentExample = new CommentExample();
+        commentExample.createCriteria().andEntityIdEqualTo(questionId)
+                .andEntityTypeEqualTo(1);
+        if (limit != 0){
+            PageHelper.offsetPage(offset,limit);
+        }
+        return commentMapper.selectByExample(commentExample);
+    }
 }
