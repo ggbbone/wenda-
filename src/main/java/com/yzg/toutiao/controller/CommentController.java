7cc975b6b829e2e4c385cdc597b37c508980a14c
ggbbone <yang847857204@gmail.com>
2019-07-18 22:59:25 +0800

    评论功能更新

--- src/main/java/com/yzg/toutiao/controller/CommentController.java
+++ src/main/java/com/yzg/toutiao/controller/CommentController.java
@@ -3,11 +3,9 @@ package com.yzg.toutiao.controller;
 import com.github.pagehelper.PageInfo;
 import com.yzg.toutiao.annotation.LoginRequired;
 import com.yzg.toutiao.aspect.LogAspect;
-import com.yzg.toutiao.model.CommentUser;
-import com.yzg.toutiao.model.HostHolder;
-import com.yzg.toutiao.model.Result;
-import com.yzg.toutiao.model.User;
+import com.yzg.toutiao.model.*;
 import com.yzg.toutiao.service.CommentService;
+import com.yzg.toutiao.service.ReplyService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,21 +23,65 @@ import java.util.List;
 @Controller
 @RequestMapping(value = "comments")
 public class CommentController {
-    private static final Logger LOGGER=LoggerFactory.getLogger(LogAspect.class);
+    private static final Logger LOGGER = LoggerFactory.getLogger(LogAspect.class);
 
     @Autowired
     CommentService commentService;
     @Autowired
+    ReplyService replyService;
+    @Autowired
     HostHolder hostHolder;
 
+    /**
+     * 查询评论下的回复
+     *
+     * @param commentId
+     * @param offset
+     * @param limit
+     * @param orderBy
+     * @param desc
+     * @return
+     */
+    @RequestMapping(value = "/{commentId}/reply")
+    @ResponseBody
+    public Result getReplies(
+            @PathVariable(value = "commentId") int commentId,
+            @RequestParam(defaultValue = "0", required = false) int offset,
+            @RequestParam(defaultValue = "5", required = false) int limit,
+            @RequestParam(defaultValue = "created_date", required = false) String orderBy,
+            @RequestParam(defaultValue = "0", required = false) int desc) {
+
+        List<ReplyUser> replies = replyService.selectRepliesByCommentId(commentId, offset,
+                limit, orderBy, desc);
+        return new Result().success().data(replies);
+    }
+
+    /**
+     * 插入1条回复
+     * @param commentId
+     * @param airId
+     * @param content
+     * @return
+     */
+    @LoginRequired
+    @RequestMapping(value = "/{commentId}/reply/{airId}", method = RequestMethod.POST)
+    @ResponseBody
+    public Result insertReply(@PathVariable(value = "commentId") int commentId,
+                              @PathVariable(value = "airId") int airId,
+                              @RequestParam String content){
+        User user = hostHolder.getUser();
+        replyService.insertReply(user.getId(),airId,commentId,content);
+
+        return new Result().success();
+    }
 
     /**
      * 查询评论
+     *
      * @param entityType 评论类型
      *                   1：问题下的回答
      *                   2：回答下的评论
-     *                   3：评论下的回复
-     * @param entityId   目标
+     * @param entityId   评论来源id(如查询问题下的回答,则是问题的id)
      * @param offset
      * @param limit
      * @return
@@ -54,17 +96,15 @@ public class CommentController {
             @RequestParam(defaultValue = "created_date", required = false) String orderBy,
             @RequestParam(defaultValue = "1", required = false) int desc) {
         List<CommentUser> comments = commentService.getComments(
-                entityType,entityId, offset, limit, orderBy, desc);
+                entityType, entityId, offset, limit, orderBy, desc);
         PageInfo<CommentUser> pageInfo = new PageInfo<>(comments);
         return new Result().success().data(pageInfo);
     }
 
     /**
-     *
      * @param entityType 评论类型
      *                   1：问题下的回答
      *                   2：回答下的评论
-     *                   3：评论下的回复
      * @param entityId   目标id
      * @param content
      * @return
@@ -77,7 +117,7 @@ public class CommentController {
             @PathVariable(value = "entityId") int entityId,
             @RequestParam String content) {
         if (hostHolder.getUser() != null) {
-            commentService.insertComment(entityType,entityId, content, hostHolder.getUser().getId());
+            commentService.insertComment(entityType, entityId, content, hostHolder.getUser().getId());
         }
         return new Result().success();
     }
